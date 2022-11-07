e6b2ab8c4ff4d7fb95db31e094de5a5b8c52cd64
kawaegle <alecromski>
2022-11-07 09:37:02 +0100

    check error with -fanalyzer (gcc12+) memoryleak 0

--- cstd_list.c
+++ cstd_list.c
@@ -7,22 +7,20 @@ int is_clist_empty(clist *l)
 
 int cstd_list_length(clist *l)
 {
-  if (is_clist_empty(l)) {
-    return (0);
-  }
   int i;
 
+    if (is_clist_empty(l))
+    return (0);
   for (i = 1; l->next != EMPTY_LIST; ++i, l = l->next);
-
   return (i);
 }
 
 clist *cstd_list_push_back(clist *l, const char *d)
 {
-  if (is_clist_empty(l)) {
-    return (EMPTY_LIST);
-  }
   clist *save = l;
+
+  if (is_clist_empty(l))
+    return (EMPTY_LIST);
   for (; l->next != EMPTY_LIST; l = l->next);
   l->next = new_clist(d);
   return (save);
@@ -30,24 +28,23 @@ clist *cstd_list_push_back(clist *l, const char *d)
 
 clist *cstd_list_push_front(clist *l, const char *d)
 {
-  if (is_clist_empty(l)) {
+  if (is_clist_empty(l))
     return (EMPTY_LIST);
-  }
+
   clist *new_node = new_clist(d);
+  if (!new_node)
+    return (l);
   new_node->next = l;
   return (new_node);
 }
 
 clist *cstd_list_at(clist *l, size_t index)
 {
-  if (is_clist_empty(l)) {
+  if (is_clist_empty(l))
     return (EMPTY_LIST);
-  }
-  for (int i = 0; l->next != EMPTY_LIST; ++i, l = l->next) {
-    if (i == index) {
+  for (size_t i = 0; l->next != EMPTY_LIST; ++i, l = l->next)
+    if (i == index)
       return (l);
-    }
-  }
   return (EMPTY_LIST);
 }
 
@@ -56,12 +53,8 @@ clist *cstd_list_pop_back(clist *l)
   char b = 'p';
   write(1, &b, 1);
 
-  if (is_clist_empty(l)) {
+  if (is_clist_empty(l))
     return (EMPTY_LIST);
-  } else if (l->len(l) == 1) {
-    free(l->data);
-    return (EMPTY_LIST);
-  }
   clist *save = l;
   for (; l->next != EMPTY_LIST; l = l->next);
   free(l->data);
@@ -70,23 +63,19 @@ clist *cstd_list_pop_back(clist *l)
 
 clist *cstd_list_pop_front(clist *l)
 {
-  if (is_clist_empty(l)) {
+  if (is_clist_empty(l))
     return (EMPTY_LIST);
-  } else if (l->len(l) == 1 && l->data != EMPTY_LIST) {
-    free(l->data);
-    return (EMPTY_LIST);
-  }
   clist *save = l->next;
   if (l->data != EMPTY_LIST) {
     free(l->data);
   }
+  free(l);
   return (save);
 }
 
 void clist_display(clist *l)
 {
   char *esp = ", ";
-
   while (l != EMPTY_LIST) {
     write(1, l->data, strlen(l->data));
     if (l->next == EMPTY_LIST) {
@@ -101,11 +90,15 @@ void clist_display(clist *l)
 clist *new_clist(const char *d)
 {
   clist *new_node = malloc(sizeof(clist));
+  if (!new_node)
+        return (EMPTY_LIST);
   new_node->data = strdup(d);
   new_node->next = EMPTY_LIST;
   new_node->push_back = &cstd_list_push_back;
   new_node->push_front = &cstd_list_push_front;
   new_node->len = &cstd_list_length;
+  new_node->pop_back = &cstd_list_pop_back;
+  new_node->pop_front = &cstd_list_pop_front;
   new_node->at = &cstd_list_at;
   return (new_node);
 }
@@ -118,18 +111,11 @@ int clist_destroy(clist *l)
   char c = 'c';
 
   size_t i = 0;
-  if (l == EMPTY_LIST) {
-    write(1, &a, 1);
-    return (i);
-  }
-
-  clist *save = l;
-  clist *h = NULL;
-  for (i = 0; l != EMPTY_LIST; ++i) {
-    if (l != NULL) {
+  for (i = 0; l != EMPTY_LIST; i++) {
+    if (l->data != NULL) {
       write(1, &b, 1);
     }
-    l = l->pop_back(l);
+    l = l->pop_front(l);
     write(1, &c, 1);
   }
   return (i);
